0ad304e21ca11602adc733643e947d1578724d2a
Mark A Brown <mark.philosophe@gmail.com>
2014-09-01 15:31:32 -0400

    added failing spec for user registration

--- test/e2e/user_registers.js
+++ test/e2e/user_registers.js
@@ -2,20 +2,41 @@
 
 describe('User registration', function() {
 
-  it('should let a new user register', function() {
+  beforeEach(function() {
     browser.get('#/register');
+  });
 
-  	var userName = element(by.model('userName')),
-  	userEmail = element(by.model('userEmail')),
+  it('should let a new user register', function() {
+
+    var userName = element(by.model('userName')),
+    userEmail = element(by.model('userEmail')),
     userPassword = element(by.model('userPassword')),
     passwordConfirmation = element(by.model('passwordConfirmation')),
-  	createUserButton = element(by.id('save-user'));
-  	
+    createUserButton = element(by.id('save-user'));
+
   	userName.sendKeys('New User');
-  	userEmail.sendKeys('mark.philosophe@gmail.com');
+  	userEmail.sendKeys('user@example.com');
     userPassword.sendKeys('secret');
     passwordConfirmation.sendKeys('secret');
   	createUserButton.click();
+
     expect(element(by.id('registration-confirmation')).getText()).toEqual("Welcome, New User! Go ahead and login!");
+  });
+
+  it('should not allow a user to register with mismatching passwords', function() {
+    
+    var userName = element(by.model('userName')),
+    userEmail = element(by.model('userEmail')),
+    userPassword = element(by.model('userPassword')),
+    passwordConfirmation = element(by.model('passwordConfirmation')),
+    createUserButton = element(by.id('save-user'));
+
+    userName.sendKeys('Another User');
+    userEmail.sendKeys('user2@example.com');
+    userPassword.sendKeys('secret');
+    userPassword.sendKeys('whoops');
+    createUserButton.click();
+
+    expect(element(by.id('error-message')).getText()).toEqual("Password and password confirmation don't match.");
   });
 });
