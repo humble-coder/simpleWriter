c97fd6da6a2b7e6b90ccbf5acbec9add130d21df
Mark A Brown <mark.philosophe@gmail.com>
2014-11-18 19:54:18 -0500

    checking redis-to-go

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ redis = require('redis');
 
 var client;
 if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-		client = redis.createClient(rtg.port, rtg.hostname);
-		client.auth(rtg.auth.split(":")[1]);
+	console.log(process.env.REDISTOGO_URL);
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	client = redis.createClient(rtg.port, rtg.hostname);
+	client.auth(rtg.auth.split(":")[1]);
 } 
 else
     client = redis.createClient();
