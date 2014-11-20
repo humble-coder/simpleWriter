6a40683151799ad08fb57886cfbfaa99e5ce887a
Mark A Brown <mark.philosophe@gmail.com>
2014-11-20 17:40:40 -0500

    and again...

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.REDISTOGO_URL) {
 	client.auth(rtg.auth.split(":")[1]);
 	salt = process.env.SALT,
 	keyString = process.env.KEYSTRING;
-	key.loadFromPem(keyString);
+	key.loadFromPEM(keyString);
 } 
 else {
   client = redis.createClient(),
